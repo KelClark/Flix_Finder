af06f24fb1aaf4d2a1935e5d9070571a4b66f69a
Dewayne Cavendish <dcavendish312@gmail.com>
2023-06-14 21:47:59 -0500

    jsx push

--- flix_finder/src/components/Movies/Movies.jsx
+++ flix_finder/src/components/Movies/Movies.jsx
@@ -1,8 +1,8 @@
-
-
 import React, { useState, useEffect } from 'react';
 import { Box, CircularProgress, useMediaQuery, Typography } from '@mui/material';
 import { useSelector } from 'react-redux';
+
+
 
 import { selectGenreOrCategory } from '../../features/currentGenreOrCategory';
 
