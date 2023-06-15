84d1300f4f02c695bf545aadf3604563a4e25891
Dewayne Cavendish <dcavendish312@gmail.com>
2023-06-14 22:00:20 -0500

    utils

--- flix_finder/src/utils/index.js
+++ flix_finder/src/utils/index.js
@@ -1,11 +1,13 @@
 import axios from 'axios';
-
 export const moviesApi = axios.create ({
     baseURL: 'https//api.themoviedb.org/3',
     params: {
         api_key: process.env.REACT_APP_TMDB_KEY,
     },
 });
+
+
+
 
 export const fetchToken = async () => {
     try {
